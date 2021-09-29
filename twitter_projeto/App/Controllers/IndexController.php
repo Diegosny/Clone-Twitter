6a5b5000459ffb794c2927c63591c6873a87a198
Diegosny <diegojamesmetalica@gmail.com>
2021-09-28 22:56:02 -0300

    Primeiro commit

---
+++ twitter_projeto/App/Controllers/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+use MF\Controller\Action;
+use MF\Model\Container;
+
+class IndexController extends Action {
+
+	public function index() {
+
+		$this->render('index');
+	}
+
+	public function inscreverse() {
+		$this->render('inscreverse');
+	}
+
+}
+
+
+?>
